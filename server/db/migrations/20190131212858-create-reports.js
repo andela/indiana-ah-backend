ee763e6884ea70aff13d3b41d86d21108e38112a
bobbylemm <chidozienwoga@gmail.com>
2019-02-01 11:39:42 +0100

    refactor: remove migration file for categories table

--- server/db/migrations/20190131212858-create-reports.js
+++ server/db/migrations/20190131212858-create-reports.js
@@ -1,30 +1,25 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Reports', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      content: {
-        type: Sequelize.STRING
-      },
-      complete: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Reports');
-  }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Reports', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    content: {
+      type: Sequelize.STRING
+    },
+    complete: {
+      type: Sequelize.BOOLEAN
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Reports')
 };
--- server/db/migrations/20190131212909-create-follows.js
+++ server/db/migrations/20190131212909-create-follows.js
@@ -1,30 +1,33 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Follows', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      content: {
-        type: Sequelize.STRING
-      },
-      complete: {
-        type: Sequelize.BOOLEAN
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Follows');
-  }
+/**
+ *
+ *
+ * @export
+ * @param {any} queryInterface
+ * @param {any} Sequelize
+ * @returns
+ */
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Follows', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    content: {
+      type: Sequelize.STRING
+    },
+    complete: {
+      type: Sequelize.BOOLEAN
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Follows')
 };
