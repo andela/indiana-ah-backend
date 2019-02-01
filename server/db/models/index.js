7dfbb59fdc528687ce046a8eb7a1d1ac28cd462c
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-02-01 16:47:56 +0100

    chore: fix code linting issues

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -10,14 +10,10 @@ const db = {};
 
 const sequelize = new Sequelize(config.url, config);
 
-fs.readdirSync(__dirname)
-  .filter(
-    file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-  )
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname).filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js').forEach((file) => {
+  const model = sequelize.import(path.join(__dirname, file));
+  db[model.name] = model;
+});
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
