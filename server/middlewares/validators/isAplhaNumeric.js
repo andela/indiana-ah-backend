bff4a6f3350dd43294c2798b35cb874a9364f06b
Akeem Balogun <balogunbiola101@gmail.com>
2019-02-06 20:01:52 +0100

    remove console log

--- server/middlewares/validators/isAplhaNumeric.js
+++ server/middlewares/validators/isAplhaNumeric.js
@@ -1,9 +1,5 @@
 export default (password) => {
-  // eslint-disable-next-line no-console
-  console.log('hahahahahahahahfdfdfdf');
   const regularExpression = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
   const valid = regularExpression.test(password);
-  // eslint-disable-next-line no-console
-  console.log('hahahahahahahah:', valid);
   return valid;
 };
