37d5c90e8cf4fcb8516d6e66b28d0647aa8d9271
omenkish <omenkish@gmail.com>
2019-02-06 12:45:58 +0100

    chore: refactor user model test

--- server/test/models/user.test.js
+++ server/test/models/user.test.js
@@ -11,7 +11,7 @@ import UserModel from '../../db/models/users';
 describe('..db/models/users', () => {
   const User = UserModel(sequelize, dataTypes);
   const user = new User();
-  checkModelName(User)('User');
+  checkModelName(User)('Users');
   // check for attributes
   context('properties', () => {
     ['id', 'username', 'name', 'email', 'imageUrl', 'password', 'bio', 'isVerified', 'subscribed']
@@ -19,40 +19,40 @@ describe('..db/models/users', () => {
   });
   // test associations
   context('associations', () => {
-    const Article = 'Article';
-    const Comment = 'Comment';
-    const Follow = 'Follow';
-    const Bookmark = 'Bookmark';
-    const Reaction = 'Reaction';
-    const Report = 'Report';
+    const Articles = 'Articles';
+    const Comments = 'Comments';
+    const Follows = 'Follows';
+    const Bookmarks = 'Bookmarks';
+    const Reactions = 'Reactions';
+    const Reports = 'Reports';
 
     before(() => {
-      User.associate({ Article });
-      User.associate({ Comment });
-      User.associate({ Follow });
-      User.associate({ Bookmark });
-      User.associate({ Reaction });
-      User.associate({ Report });
+      User.associate({ Articles });
+      User.associate({ Comments });
+      User.associate({ Follows });
+      User.associate({ Bookmarks });
+      User.associate({ Reactions });
+      User.associate({ Reports });
     });
 
     it('should define a hasMany association with Article', () => {
-      expect(User.hasMany.calledWith(Article)).to.equal(true);
+      expect(User.hasMany.calledWith(Articles)).to.equal(true);
     });
 
     it('should define a hasMany association with Comment', () => {
-      expect(User.hasMany.calledWith(Comment)).to.equal(true);
+      expect(User.hasMany.calledWith(Comments)).to.equal(true);
     });
     it('should define a hasMany association with Bookmark', () => {
-      expect(User.hasMany.calledWith(Bookmark)).to.equal(true);
+      expect(User.hasMany.calledWith(Bookmarks)).to.equal(true);
     });
     it('should define a hasMany association with Follow', () => {
-      expect(User.hasMany.calledWith(Follow)).to.equal(true);
+      expect(User.hasMany.calledWith(Follows)).to.equal(true);
     });
     it('should define a hasMany association with Reaction', () => {
-      expect(User.hasMany.calledWith(Reaction)).to.equal(true);
+      expect(User.hasMany.calledWith(Reactions)).to.equal(true);
     });
     it('should define a hasMany association with Report', () => {
-      expect(User.hasMany.calledWith(Report)).to.equal(true);
+      expect(User.hasMany.calledWith(Reports)).to.equal(true);
     });
   });
 });
