dbe1ec7116d844e6466180338975e7eeb285829e
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-02-07 07:17:01 +0100

    feat: write tests for article crud operations

---
+++ server/tests/app/mockData/articlesMockData.js
@@ -0,0 +1,22 @@
+export const invalidArticle = {
+  articleTitle: '',
+  articleBody: 'uwuuwuwwuywuuiwiwiwwiiwiwiiiwiwiwiiw',
+  tags: ''
+};
+
+export const validArticle = {
+  articleTitle: 'How I got into Andela',
+  articleBody: 'djdjjdjdjjdjdjjdjdjdjjdjdjjjd'
+};
+
+export const articleForUpdate = {
+  articleTitle: 'How I got into Andela',
+  articleBody: 'djdjjdjdjjdjdjjdjdjdjjdjdjjjd',
+  tags: 'yes, no, true'
+};
+
+export const user = {
+  username: 'ozone4real',
+  email: 'ezenwa9000@gmail.com',
+  password: 'baleesecret'
+};
