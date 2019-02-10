ebf8518b8123debea670d83a0bc19ff08bb007a5
Tiku Okoye <32392378+primuse@users.noreply.github.com>
2019-02-10 16:10:11 +0100

    #163426525 View and edit user profile (#17)
    feat: view and edit user profile

---
+++ server/helpers/jwtHelper.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+/**
+* @description A collection of helper methods for JWT operations
+* @class JWTHelper
+*/
+class JWTHelper {
+  /**
+   * @description helper method for verifying a jwt token
+   * @static
+   * @param {String} token to be verified
+   * @returns {Object} a user payload or an error
+   */
+  static verifyToken(token) {
+    if (!token) return false;
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      return decodedToken;
+    } catch (error) {
+      if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') return false;
+    }
+  }
+}
+
+export default JWTHelper;
