ab2bbf9f9745f1442cc7c14fc413d3405c704211
Efe <knowledgeagare157@gmail.com>
2019-02-11 17:06:13 +0100

    fix: fix typo in commentAnArticle catch block

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -19,7 +19,6 @@ class CommentController {
   static async commentAnArticle(req, res) {
     try {
       const { slug } = req.params;
-
       const { id: userId } = req.user;
       req.body.userId = userId;
       const article = await Articles.findOne({
@@ -33,14 +32,13 @@ class CommentController {
         });
       }
       req.body.articleId = article.dataValues.id;
-
       const Articlecomments = await Comments.create(req.body);
       return res.status(201).json({
         success: true,
         message: 'Comment has been posted successfully',
         data: Articlecomments
       });
-    } catch (e) {
+    } catch (error) {
       return errorMessage(res, 500, 'internal server error');
     }
   }
