0ec9b77b9ab49a3fcd287708b3cbe207dc0da61d
omenkish <omenkish@gmail.com>
2019-02-11 19:36:14 +0100

    feat: update assignJwtToken.js

--- server/helpers/assignJwtToken.js
+++ server/helpers/assignJwtToken.js
@@ -2,9 +2,6 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-// const duration = { expiresIn: '24hrs' };
-// const secretKey = process.env.JWT_SECRET_KEY;
 const assignToken = (payload, secretKey = process.env.JWT_SECRET_KEY, duration = { expiresIn: '24hrs' }) => jwt.sign(payload, secretKey, duration);
 
 export default assignToken;
