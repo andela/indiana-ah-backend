9154d4915d262dcb44e72c6f401d0bd3b2856a11
omenkish <omenkish@gmail.com>
2019-02-12 08:50:06 +0100

    feat: write test cases for comment reactions model

---
+++ server/tests/models/commentReactions.test.js
@@ -0,0 +1,38 @@
+import {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists
+} from 'sequelize-test-helpers';
+import { expect } from 'chai';
+
+import CommentReactionModel from '../../db/models/commentreactions';
+
+describe.only('..db/models/commentreactions', () => {
+  const CommentReaction = CommentReactionModel(sequelize, dataTypes);
+  const commentReaction = new CommentReaction();
+  checkModelName(CommentReaction)('CommentReactions');
+  // check for attributes
+  context('properties', () => {
+    ['id', 'reactionType', 'userId', 'commentId']
+      .forEach(checkPropertyExists(commentReaction));
+  });
+  // test associations
+  context('associations', () => {
+    const Comments = 'Comments';
+    const Users = 'Users';
+
+    before(() => {
+      CommentReaction.associate({ Comments });
+      CommentReaction.associate({ Users });
+    });
+
+    it('should define a belongsTo association with Article', () => {
+      expect(CommentReaction.belongsTo.calledWith(Comments)).to.equal(true);
+    });
+
+    it('should define a belongsTo association with User', () => {
+      expect(CommentReaction.belongsTo.calledWith(Users)).to.equal(true);
+    });
+  });
+});
