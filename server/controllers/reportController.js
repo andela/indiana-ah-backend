ca4e1eebbe5041cd46eafb86de69628f6504b60c
Efe <knowledgeagare157@gmail.com>
2019-02-12 13:29:13 +0100

    feat: create user report class

--- server/controllers/reportController.js
+++ server/controllers/reportController.js
@@ -1,43 +1,40 @@
 import models from '../db/models';
-import errorMessage from '../helpers/errorHelpers';
 
-const { Comments, Articles } = models;
+const { Reports, Articles } = models;
 
 /**
- * @description  Handles Users comments on articles
+ * @description  Handles Users reports on articles
  * @class CommentController
  */
-class CommentController {
+class ReportController {
   /**
    * @description controller method for commenting on an article
    * @static
    * @param {object} req Request object
    * @param {object} res Response object
+   * @param {object} next Response object
    * @returns {Object} a response object
    */
-  static async articleComment(req, res) {
+  static async reportArticle(req, res, next) {
     try {
       req.body.userId = req.user.id;
       const { slug } = req.params;
-      const article = await Articles.findOne({
-        where: { slug },
-        returning: true
-      });
+      const article = await Articles.findOne({ where: { slug }, returning: true });
       if (!article) {
         return res.status(404).json({
           message: 'Article not found'
         });
       }
       req.body.articleId = article.dataValues.id;
-      const articleComments = await Comments.create(req.body);
+      const reportArticle = await Reports.create(req.body);
       return res.status(201).json({
         message: 'Comment has been posted successfully',
-        data: articleComments
+        data: reportArticle
       });
     } catch (error) {
-      return errorMessage(res, 500, 'internal server error');
+      return next(error);
     }
   }
 }
 
-export default CommentController;
+export default ReportController;
