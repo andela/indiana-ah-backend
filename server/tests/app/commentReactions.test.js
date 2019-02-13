d9d33fe2fbc0c0a7e02e22a8b842d3d3bf1bdb51
omenkish <omenkish@gmail.com>
2019-02-13 12:19:13 +0100

    feat: refactor test cases for like and dislike comment feature

--- server/tests/app/commentReactions.test.js
+++ server/tests/app/commentReactions.test.js
@@ -90,4 +90,65 @@ describe('Comment Reactions', () => {
         expect(res.body.message).to.equal('This is not an allowed reaction type');
       }));
   });
+<<<<<<< HEAD
+=======
+=======
+before(async () => {
+  await Users.create(user);
+  return request(app)
+    .post('/api/v1/login')
+    .send({ email: user.email, password: user.password })
+    .then((res) => {
+      userToken = res.body.token;
+    });
+});
+describe('Create an article', () => {
+  it('create an article if the user passes authentication', () => request(app)
+    .post('/api/v1/articles')
+    .set('x-auth-token', userToken)
+    .send(article)
+    .then((res) => {
+      articleSlug = res.body.article.slug;
+      expect(res.status).to.equal(201);
+      expect(res.body.article).to.be.an('object');
+    }));
+});
+
+describe('Create a comment', () => {
+  it('create a comment if the user passes authentication', () => request(app)
+    .post(`/api/v1/articles/${articleSlug}/comments`)
+    .set('x-auth-token', userToken)
+    .send(comment)
+    .then((res) => {
+      commentId = res.body.data.id;
+      expect(res.status).to.equal(201);
+      expect(res.body.data).to.be.an('object');
+    }));
+});
+
+describe('Like and Dislike comments', () => {
+  it('should return an error if user is not authenticated', () => request(app)
+    .post('/api/v1/comments/reaction')
+    .then((res) => {
+      expect(res.status).to.equal(401);
+      expect(res.body.message).to.equal('Access denied. You are not authorized to access this route');
+    }));
+  it('should successfully like an article if user is authenticated', () => request(app)
+    .post('/api/v1/comments/reaction')
+    .set('x-auth-token', userToken)
+    .send({ commentId, reactionType: 'like' })
+    .then((res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body.message).to.equal('You have successfully liked');
+    }));
+  it('should successfully dislike an article if user is authenticated', () => request(app)
+    .post('/api/v1/comments/reaction')
+    .set('x-auth-token', userToken)
+    .send({ commentId, reactionType: 'dislike' })
+    .then((res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body.message).to.equal('You have successfully disliked');
+    }));
+>>>>>>> feat: implement test cases for like and dislike comment
+>>>>>>> feat: refactor test cases for like and dislike comment feature
 });
