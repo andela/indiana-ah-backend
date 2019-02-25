b5f501e28175de0510e4608b4ad29f1472b0352f
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-02-25 10:44:22 +0100

    fix: fix reset-password feature and validate reset password

--- server/helpers/baseHelper.js
+++ server/helpers/baseHelper.js
@@ -1,5 +1,4 @@
 import bcrypt from 'bcrypt';
-import logger from '../winstonConfig';
 /**
  * @class BaseHelper
  */
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import UserController from '../controllers/userController';
 import signUpValidator from '../middlewares/validators/signUpValidator';
+import resetPasswordValidator from '../middlewares/validators/resetPasswordValidator';
 import userValidator from '../middlewares/validators/userValidator';
 import jwtAuth from '../middlewares/jwtAuthentication';
 import parser from '../cloudinaryConfig';
@@ -36,7 +37,7 @@ router.patch('/profiles/:username/update', jwtAuth.authUser, validateUser, userV
 router.patch('/profiles/:username/image', jwtAuth.authUser, validateUser, parser.single('image'), uploadUserPicture);
 router.delete('/profiles/:username/delete', jwtAuth.authUser, validateUser, deleteUserProfile);
 router.post('/users/begin-password-reset', sendPasswordResetLink);
-router.patch('/users/reset-password', resetPassword);
+router.patch('/users/reset-password', resetPasswordValidator, resetPassword);
 router.post('/profiles/:username/follow', jwtAuth.authUser, follow);
 router.get('/profiles/users/following', jwtAuth.authUser, fetchFollowing);
 router.get('/profiles/users/followers', jwtAuth.authUser, fetchFollowers);
