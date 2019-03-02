2fcaa1b9e91c5ab3fdc34c720087e7cdcc8d79df
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-03-02 18:53:19 +0100

    fix: fix validation schema for articles

--- server/middlewares/validators/validationSchemas.js
+++ server/middlewares/validators/validationSchemas.js
@@ -7,9 +7,8 @@ export const articleSchema = {
   articleBody: Joi.string()
     .min(10)
     .required(),
-  tags: Joi.array()
-    .items(Joi.string())
-    .min(2),
+  tags: Joi.string().min(2),
+  imageUrl: Joi.string().uri()
 };
 
 export const ratingSchema = {
