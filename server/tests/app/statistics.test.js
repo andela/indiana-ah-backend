64af479b64076cc2134049f445f99f0bb21998fb
bobbylemm <chidozienwoga@gmail.com>
2019-03-05 13:58:57 +0100

    feat: implement get user statistics

--- server/tests/app/statistics.test.js
+++ server/tests/app/statistics.test.js
@@ -1,23 +1,24 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import app from '../../index';
-import { user1 } from './mockData/articlesMockData';
+import user from './mockData/reactionsMockData';
 
 let verifiedToken;
 
 before(async () => request(app)
   .post('/api/v1/login')
   .set('content-type', 'application/json')
-  .send({ email: user1.email, password: user1.password })
+  .send(user)
   .then((res) => {
     verifiedToken = res.body.token;
   }));
 
 describe('Get a user statistics', () => {
   it('should return statistics for a user', () => request(app)
-    .get('/api/v1/statistics')
+    .get('/api/v1/users/statistics')
     .set('x-auth-token', verifiedToken)
     .then((res) => {
       expect(res.status).to.equal(200);
+      expect(res.body.message.statistics).to.haveOwnProperty('reactionStat');
     }));
 });
