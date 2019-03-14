247dd00abe646d2e728fd2c23cb2a9058ae7cf77
divinediscipline <31392460+divinediscipline@users.noreply.github.com>
2019-03-14 15:13:24 +0100

    bug: fix wrong redirect for social authentication #164559308

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -516,7 +516,7 @@ class UserController extends BaseHelper {
       isVerified
     };
     const token = assignToken(payload);
-    return res.redirect(`/?token=${token}`);
+    return res.redirect(`${process.env.FRONTEND_URL}/social-auth?token=${token}`);
   }
 
   /**
